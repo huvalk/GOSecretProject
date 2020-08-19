59a1ad36b5cf2de76843c573d05061986eac70cd
Куклин С. И <akiraq@yandex.ru>
2020-08-19 20:42:17 +0300

    fix(docker): no docker

--- core/server/app.go
+++ core/server/app.go
@@ -28,9 +28,7 @@ func (app *App) StartRouter() {
 
 	port := 8001
 	golog.Infof("Server started at port :%d", port)
-	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port),
-		"/etc/letsencrypt/live/hahao.ru/fullchain.pem",
-		"/etc/letsencrypt/live/hahao.ru/privkey.pem",
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port),
 		nil)
 
 	if err != nil {
