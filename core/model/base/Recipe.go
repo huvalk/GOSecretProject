cd85923f97382990fcf9769ed98ba046eccc4e59
Marat <maratishimbaev8@gmail.com>
2020-10-24 16:28:29 +0300

    feat: chnge json name

--- core/model/base/Recipe.go
+++ core/model/base/Recipe.go
@@ -4,7 +4,7 @@ type Recipe struct {
 	Id          uint64   `json:"id"`
 	Author      uint64   `json:"author"`
 	Title       string   `json:"title"`
-	CookingTime uint64   `json:"cooking_time"`
+	CookingTime uint64   `json:"cookingTime"`
 	Ingredients []string `json:"ingredients"`
 	Steps       []string `json:"steps"`
 }
